7b67b2408e9c385ccfe990f461f724cd21a65680
Valida258 <qizi.qizo@gmail.com>
2026-06-25 06:22:11 +0400

    fix: admin_token duzeldildi

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+        'Authorization': `Bearer ${localStorage.getItem('admin_token')}`
     },
     timeout: 10000
 });
